de35db9ac3bcea44986dfd26954345a17b05647a
Alexandre Chopin <wikeo65@gmail.com>
2017-02-11 13:02:36 +0100

    add build

--- build/build.js
+++ build/build.js
@@ -1,36 +1,73 @@
-// https://github.com/shelljs/shelljs
-require('./check-versions')()
-require('shelljs/global')
-env.NODE_ENV = 'production'
+const rollup = require('rollup').rollup
+const vue = require('rollup-plugin-vue')
+const jsx = require('rollup-plugin-jsx')
+const buble = require('rollup-plugin-buble')
+const replace = require('rollup-plugin-replace')
+const uglify = require('uglify-js')
+const CleanCSS = require('clean-css')
+const packageData = require('../package.json')
+const { version, author, name } = packageData
+// remove the email at the end
+const authorName = author.replace(/\s+<.*/, '')
 
-var path = require('path')
-var config = require('../config')
-var ora = require('ora')
-var webpack = require('webpack')
-var webpackConfig = require('./webpack.docs.conf')
+const {
+  logError,
+  write,
+  processStyle
+} = require('./utils')
 
-console.log(
-  '  Tip:\n' +
-  '  Built files are meant to be served over an HTTP server.\n' +
-  '  Opening index.html over file:// won\'t work.\n'
-)
+const banner =
+      '/*!\n' +
+      ` * ${name} v${version}\n` +
+      ` * (c) ${new Date().getFullYear()} ${authorName}\n` +
+      ' * Released under the MIT License.\n' +
+      ' */'
 
-var spinner = ora('building for production...')
-spinner.start()
-
-var assetsPath = path.join(config.docs.assetsRoot, config.docs.assetsSubDirectory)
-rm('-rf', assetsPath)
-mkdir('-p', assetsPath)
-cp('-R', 'static/*', assetsPath)
-
-webpack(webpackConfig, function (err, stats) {
-  spinner.stop()
-  if (err) throw err
-  process.stdout.write(stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false
-  }) + '\n')
-})
+rollup({
+  entry: 'src/index.js',
+  plugins: [
+    vue({
+      compileTemplate: true,
+      css (styles, stylesNodes) {
+        Promise.all(
+          stylesNodes.map(processStyle)
+        ).then(css => {
+          const result = css.map(c => c.css).join('')
+          // write the css for every component
+          // TODO add it back if we extract all components to individual js
+          // files too
+          // css.forEach(writeCss)
+          write(`dist/${name}.css`, result)
+          write(`dist/${name}.min.css`, new CleanCSS().minify(result).styles)
+        }).catch(logError)
+      }
+    }),
+    jsx({ factory: 'h' }),
+    replace({
+      __VERSION__: version
+      // TODO make sure to produce two libs: one for the browser and one for common.js
+      // the same way Vue does
+      // 'process.env.NODE_ENV': '"production"'
+    }),
+    buble()
+  ]
+}).then(function (bundle) {
+  var code = bundle.generate({
+    format: 'umd',
+    exports: 'named',
+    banner: banner,
+    moduleName: name
+  }).code
+  return write(`dist/${name}.js`, code).then(function () {
+    return code
+  })
+}).then(function (code) {
+  var minified = uglify.minify(code, {
+    fromString: true,
+    output: {
+      preamble: banner,
+      ascii_only: true // eslint-disable-line camelcase
+    }
+  }).code
+  return write(`dist/${name}.min.js`, minified)
+}).catch(logError)
