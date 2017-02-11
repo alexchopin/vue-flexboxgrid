a39fecd22cd009d080277a2599fc000c71623d44
Alexandre Chopin <wikeo65@gmail.com>
2017-02-11 12:55:30 +0100

    delete old boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,9 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  extends: 'vue',
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
