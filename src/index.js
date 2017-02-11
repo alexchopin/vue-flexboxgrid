70a32b5c617f22b2ffa4d53e3c3979217898584d
Alexandre Chopin <wikeo65@gmail.com>
2017-02-11 13:03:25 +0100

    add components index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import Container from './components/Container'
-import Column from './components/Column'
-import Row from './components/Row'
+import Container from './components/Container.vue'
+import Row from './components/Row.vue'
+import Column from './components/Column.vue'
 
-// export default Container
-// export default Column
-// export default Row
+function plugin (Vue) {
+  Vue.component('container', Container)
+  Vue.component('row', Row)
+  Vue.component('column', Column)
+}
 
-export { Container, Column, Row }
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__'
+// Export all components too
+export {
+  Container,
+  Row,
+  Column,
+  version
+}
